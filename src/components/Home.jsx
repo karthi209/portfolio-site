cfab7d4974fbcdc16143bf594e9dec98ff146394
Karthikeyan Manimaran <karthikeyan14june@gmail.com>
2024-07-14 17:52:36 +0530

    new commits

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,15 @@ import React from 'react';
 const Home = () => {
   return(
     <>
-      <p className='coolPara'>Hey! This is me. I do a little bit of everything form DevOps to Web Dev to Kernel Dev to map making.</p>
+      <div className="split right" >
+        <div className="coolPara centered">
+           <p>I love building software, doesn't matter if it's a website or a low level kernel utiliy tool. I love the fact that we can write a bunch of instructions and the computer can execute them  flawlessly, as it's written. It's like having superpowers. </p>
+           <p>I mostly spend my work time in building and maintaining platform infrastrucre for the teams acroos to use. In my typical day, I'll have my teminal open with k9s running, so I can always take a quickl peek at how things are going with the Kubernetes clusters. </p>
+        </div>
+      </div>
+      <div className="split left">
+        <p className="centered"> my bio </p>
+      </div>
     </>
   );
 };
